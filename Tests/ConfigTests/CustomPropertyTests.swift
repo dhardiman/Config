aa07a7f593e387fa637ca957b9a6bf215a791f09
David Hardiman <david_hardiman@yahoo.co.uk>
2019-04-24 16:32:28 +0100

    Tests for custom property array

--- Tests/ConfigTests/CustomPropertyTests.swift
+++ Tests/ConfigTests/CustomPropertyTests.swift
@@ -11,52 +11,6 @@ import Nimble
 import XCTest
 
 class CustomPropertyTests: XCTestCase {
-    func givenACustomType(for dictionary: [String: Any] = givenATypeDictionary()) -> CustomType {
-        return CustomType(source: dictionary)!
-    }
-
-    func givenADictionary() -> [String: Any] {
-        return [
-            "description": "A description",
-            "defaultValue": "test default value",
-            "overrides": [
-                "override": "overridden value"
-            ]
-        ]
-    }
-
-    func givenADictionaryWithValues() -> [String: Any] {
-        return [
-            "description": "A description",
-            "defaultValue": [
-                "firstplaceholder": "firstValue",
-                "secondplaceholder": "secondValue"
-            ],
-            "overrides": [
-                "override": [
-                    "firstplaceholder": "firstOverriddenValue",
-                    "secondplaceholder": "secondOverriddenValue"
-                ]
-            ]
-        ]
-    }
-
-    func givenADictionaryWithTypedValues() -> [String: Any] {
-        return [
-            "description": "A description",
-            "defaultValue": [
-                "firstplaceholder": "firstValue",
-                "secondplaceholder": true
-            ],
-            "overrides": [
-                "override": [
-                    "firstplaceholder": "firstOverriddenValue",
-                    "secondplaceholder": false
-                ]
-            ]
-        ]
-    }
-
     func testItIsCreatedCorrectly() {
         let property = CustomProperty(key: "test", customType: givenACustomType(), dict: givenADictionary())
         expect(property.key).to(equal("test"))
@@ -132,4 +86,116 @@ class CustomPropertyTests: XCTestCase {
         """
         expect(property.propertyDeclaration(for: "any", iv: try IV(dict: [:]), encryptionKey: nil, requiresNonObjCDeclarations: false, isPublic: true, indentWidth: 0)).to(equal(expectedValue))
     }
+}
+
+class CustomPropertyArrayTests: XCTestCase {
+    func testItIsCreatedCorrectly() {
+        let property = CustomPropertyArray(key: "test", customType: givenACustomType(), dict: givenADictionaryWithSimpleArrayValues())
+        expect(property.key).to(equal("test"))
+        expect(property.description).to(equal("A description"))
+        expect(property.customType.typeName).to(equal("CustomType"))
+        expect(property.defaultValue as? [String]).to(equal(["test default value"]))
+        expect(property.overrides as NSDictionary).to(equal([
+            "override": ["overridden value"]
+            ] as NSDictionary))
+    }
+
+    func testItReportsItsTypeName() {
+        let property = CustomPropertyArray(key: "test", customType: givenACustomType(), dict: givenADictionaryWithSimpleArrayValues())
+        expect(property.typeName).to(equal("[CustomType]"))
+    }
+
+    func testItOutputsAPropertyForATypeWithTypeAnnotations() {
+        let property = CustomPropertyArray(key: "test", customType: givenACustomType(for: givenATypeDictionaryWithTypeAnnotations()), dict: givenADictionaryWithArrayValues())
+        let expectedValue = """
+            /// A description
+            public static let test: [CustomType] = [CustomType(oneThing: "firstValue", secondThing: true)]
+        """
+        expect(property.propertyDeclaration(for: "any", iv: try IV(dict: [:]), encryptionKey: nil, requiresNonObjCDeclarations: false, isPublic: true, indentWidth: 0)).to(equal(expectedValue))
+    }
+
+    func testItOutputsAPropertyForAnOverriddenScheme() {
+        let property = CustomPropertyArray(key: "test", customType: givenACustomType(for: givenATypeDictionaryWithTypeAnnotations()), dict: givenADictionaryWithArrayValues())
+        let expectedValue = """
+            /// A description
+            public static let test: [CustomType] = [CustomType(oneThing: "firstOverriddenValue", secondThing: false)]
+        """
+        expect(property.propertyDeclaration(for: "override", iv: try IV(dict: [:]), encryptionKey: nil, requiresNonObjCDeclarations: false, isPublic: true, indentWidth: 0)).to(equal(expectedValue))
+    }
+}
+
+private func givenACustomType(for dictionary: [String: Any] = givenATypeDictionary()) -> CustomType {
+    return CustomType(source: dictionary)!
+}
+
+private func givenADictionary() -> [String: Any] {
+    return [
+        "description": "A description",
+        "defaultValue": "test default value",
+        "overrides": [
+            "override": "overridden value"
+        ]
+    ]
+}
+
+private func givenADictionaryWithSimpleArrayValues() -> [String: Any] {
+    return [
+        "description": "A description",
+        "defaultValue": ["test default value"],
+        "overrides": [
+            "override": ["overridden value"]
+        ]
+    ]
+}
+
+private func givenADictionaryWithValues() -> [String: Any] {
+    return [
+        "description": "A description",
+        "defaultValue": [
+            "firstplaceholder": "firstValue",
+            "secondplaceholder": "secondValue"
+        ],
+        "overrides": [
+            "override": [
+                "firstplaceholder": "firstOverriddenValue",
+                "secondplaceholder": "secondOverriddenValue"
+            ]
+        ]
+    ]
+}
+
+private func givenADictionaryWithArrayValues() -> [String: Any] {
+    return [
+        "description": "A description",
+        "defaultValue": [
+            [
+                "firstplaceholder": "firstValue",
+                "secondplaceholder": true
+            ]
+        ],
+        "overrides": [
+            "override": [
+                [
+                    "firstplaceholder": "firstOverriddenValue",
+                    "secondplaceholder": false
+                ]
+            ]
+        ]
+    ]
+}
+
+private func givenADictionaryWithTypedValues() -> [String: Any] {
+    return [
+        "description": "A description",
+        "defaultValue": [
+            "firstplaceholder": "firstValue",
+            "secondplaceholder": true
+        ],
+        "overrides": [
+            "override": [
+                "firstplaceholder": "firstOverriddenValue",
+                "secondplaceholder": false
+            ]
+        ]
+    ]
 }
