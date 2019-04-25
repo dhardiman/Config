76e17e4522de778fb8aa5757448ca02366f6b07a
David Hardiman <david_hardiman@yahoo.co.uk>
2019-04-25 08:40:44 +0100

    Add test for default filename

---
+++ Tests/ConfigTests/TemplateTests.swift
@@ -0,0 +1,29 @@
+//
+//  TemplateTests.swift
+//  ConfigTests
+//
+//  Created by David Hardiman on 25/04/2019.
+//
+
+@testable import Config
+import Foundation
+import Nimble
+import XCTest
+
+class TemplateTests: XCTestCase {
+    func testThereIsADefaultFilename() throws {
+        let template = try TestTemplate(config: [:], name: "", scheme: "", source: URL(fileURLWithPath: "/"))
+        expect(template.filename).to(beNil())
+    }
+}
+
+struct TestTemplate: Template {
+    init(config: [String : Any], name: String, scheme: String, source: URL) throws {
+    }
+
+    static func canHandle(config: [String : Any]) -> Bool {
+        return false
+    }
+
+    let description = "TestTemplate"
+}
